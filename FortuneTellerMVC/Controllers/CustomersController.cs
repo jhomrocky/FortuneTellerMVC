e49c909e25c11315c59e612e556ae2fccf419d4f
jhomrocky <jhomrocky@gmail.com>
2017-11-01 14:41:19 -0400

    Done

--- FortuneTellerMVC/Controllers/CustomersController.cs
+++ FortuneTellerMVC/Controllers/CustomersController.cs
@@ -33,6 +33,96 @@ namespace FortuneTellerMVC.Controllers
             {
                 return HttpNotFound();
             }
+
+            //Retirement age based on customer age
+            if (customer.Age % 2 == 0)
+            {
+                ViewBag.RetirementAge = "15 years";
+            }
+            else if (customer.Age %2 == 1)
+            {
+                ViewBag.RetirementAge = "50 years";
+            }
+            else
+            {
+                ViewBag.RetirementAge = "never";
+            }
+
+            //Vacation Home based on number of siblings
+           if (customer.NumberOfSiblings == 0)
+            {
+                ViewBag.VacationHome = "Hawaii";
+            }
+            else if (customer.NumberOfSiblings == 1)
+            {
+                ViewBag.VacationHome = "Florida";
+            }
+            else if (customer.NumberOfSiblings == 2)
+            {
+                ViewBag.VacationHome = "Thailand";
+            }
+            else if (customer.NumberOfSiblings == 3)
+            {
+                ViewBag.VacationHome = "Paris";
+            }
+            else if (customer.NumberOfSiblings > 3)
+            {
+                ViewBag.VacationHome = "Greece";
+            }
+            else
+            {
+                ViewBag.VacationHome = "Dumpster";
+            }
+
+            // Money in the bank based on birth month
+            if (customer.BirthMonthID > 0 && customer.BirthMonthID <= 4)
+            {
+                ViewBag.BankMoney = 1000000;
+            }
+            else if (customer.BirthMonthID > 4 && customer.BirthMonthID <= 8)
+            {
+                ViewBag.BankMoney = 500000;
+            }
+            else if (customer.BirthMonthID > 8 && customer.BirthMonthID <= 12)
+            {
+                ViewBag.BankMoney = 250000;
+            }
+            else
+            {
+                ViewBag.BankMoney = 0;
+            }
+
+            //Transportation based on favorite color
+
+            if (customer.FavoriteColorID == 1)
+            {
+                ViewBag.RideType = "jetpack";
+            }
+            else if (customer.FavoriteColorID == 2)
+            {
+                ViewBag.RideType = "personal helicopter";
+            }
+            else if (customer.FavoriteColorID == 3)
+            {
+                ViewBag.RideType = "sports car";
+            }
+            else if (customer.FavoriteColorID == 4)
+            {
+                ViewBag.RideType = "speedboat";
+            }
+            else if (customer.FavoriteColorID == 5)
+            {
+                ViewBag.RideType = "motorcycle";
+            }
+            else if (customer.FavoriteColorID == 6)
+            {
+                ViewBag.RideType = "segway";
+            }
+            else if (customer.FavoriteColorID == 7)
+            {
+                ViewBag.RideType = "submarine";
+            }
+
             return View(customer);
         }
 
